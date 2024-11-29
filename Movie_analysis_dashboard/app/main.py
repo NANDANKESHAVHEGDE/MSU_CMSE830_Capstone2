26859ca0b9325397b7c5f5a7b328a55c8e2737b1
Nandan Keshav Hedge <nandankeshavhegde@gmail.com>
2024-11-29 01:53:30 -0500

    psuhing first version of streamlit cloud

--- Movie_analysis_dashboard/app/main.py
+++ Movie_analysis_dashboard/app/main.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-os.system('pip install joblib')
-import joblib
 import joblib
 import seaborn as sns
 import matplotlib.pyplot as plt
