6e7c3e513b611be84ea39ebea2c15e9a6b3d9733
Nandan Keshav Hedge <nandankeshavhegde@gmail.com>
2024-11-29 01:47:42 -0500

    pushing code for first version deployment of app in streamcloud

--- Movie_analysis_dashboard/app/main.py
+++ Movie_analysis_dashboard/app/main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
+os.system('pip install joblib')
+import joblib
 import joblib
 import seaborn as sns
 import matplotlib.pyplot as plt
